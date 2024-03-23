1a2eadc8a2fb6930d5f39d76a5d09d7600b2c9f9
precy001 <146995543+precy001@users.noreply.github.com>
2024-03-23 17:32:57 +0100

    Update Home.js

--- scripts/Home.js
+++ scripts/Home.js
@@ -1,18 +1,4 @@
-const images = ['assets/images/background1.png', 'assets/images/background2.png', 'assets/images/background3.png']
-const body = document.querySelector('.home')
-let currentIndex = 0
 
-function changeBackground(){
-    currentIndex ++
-    body.style.backgroundImage = `url(${images[currentIndex]})`
-    /* textElement.innerhtml = `${text[currentIndex]}` */
-    if(currentIndex === 2){
-        currentIndex = -1
-    }
-}
-
-changeBackground();
-setInterval(changeBackground, 8000)
 
 const menuBtn = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-nav')
